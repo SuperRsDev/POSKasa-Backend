a9803c434d471e10709fde273077b6a59228dc97
apozegija1 <apozegija1@etf.unsa.ba>
2020-05-19 10:29:23 +0200

    Ubačeni početni podaci za potrebe testiranja REST zahtjeva.

--- modules/core/server/database/dbSetup.js
+++ modules/core/server/database/dbSetup.js
@@ -18,7 +18,7 @@ function inicializacija(){
     var userPromise=[ db.user.create(
         {
             firstName: 'Amra',
-            lastName: 'Pozeijga',
+            lastName: 'Pozegija',
             username: 'apozegija1',
             password: 'amraamra',
             email: 'apozegija1@etf.unsa.ba',
@@ -40,7 +40,9 @@ function inicializacija(){
         db.role.create({
             description: 'radnik, bez privilegija brisanja i dodavanja',
             name: 'radnik'})
+
     ];
+
     var userRolePromise = [ db.userRole.create(
         {
             roleId: 1,
@@ -48,11 +50,104 @@ function inicializacija(){
         }
     )];
 
+    var categoryPromise = [
+        db.category.create({
+            description: 'kategorija 1',
+            name: 'Hrana'}),
+        db.category.create({
+            description: 'kategorija 2',
+            name: 'Pice'})
+
+    ];
+
+    var productPromise = [
+        db.product.create({
+            name: 'Cips Chipsy classic',
+            stockQuantity: 200,
+            status: 'dostupan',
+            description: 'cips slani',
+            unitPrice: 1,
+            sellingPrice: 2,
+            categoryId: 1
+        }),
+        db.product.create({
+            name: 'Ledeni caj',
+            stockQuantity: 100,
+            status: 'dostupan',
+            description: 'okus sumsko voce',
+            unitPrice: 1,
+            sellingPrice: 2,
+            categoryId: 2
+        })
+    ];
+
+    var paymentTypePromise = [
+        db.paymentType.create({
+            paymentTypeProvider: 'gotovina',
+            description: ''
+        })
+    ];
+
+    var employeePromise = [
+        db.employee.create({
+            userId: 1,
+            managerId: 1,
+            hireDate: '1.1.2019',
+            jobTitle: 'konobar'
+        })
+    ];
+
+    var orderPromise = [
+        db.order.create({
+            employeeId: 1,
+            paymentTypeId: 1,
+            date: '19.5.2020',
+            status: '',
+            orderType: ''
+        })
+    ];
+
+    var productOrderPromise = [
+        db.productOrder.create({
+            orderId: 1,
+            productId: 1,
+            quantity: 3,
+        }),
+        db.productOrder.create({
+            orderId: 1,
+            productId: 2,
+            quantity: 2,
+        })
+            .then(function(user) {
+            // you can now access the newly created user
+            console.log('success', user.toJSON());
+            })
+            .catch(function(err) {
+                // print the error details
+                console.log(err, request.body);
+            })
+    ];
+
+
+    var posPromise = [
+        db.pos.create({
+            orderId: 1,
+            totalSum: 5,
+            fiscalNumber: 123,
+        }),
+    ];
 
     return new Promise((resolve, reject) => {
         Promise.all(userPromise)
             .then(() => Promise.all(rolePromise).then(all => resolve(all)))
             .then(() => Promise.all(userRolePromise).then(all => resolve(all)))
+            .then(() => Promise.all(categoryPromise).then(all => resolve(all)))
+            .then(() => Promise.all(productPromise).then(all => resolve(all)))
+            .then(() => Promise.all(paymentTypePromise).then(all => resolve(all)))
+            .then(() => Promise.all(employeePromise).then(all => resolve(all)))
+            .then(() => Promise.all(orderPromise).then(all => resolve(all)))
+            .then(() => Promise.all(productOrderPromise).then(all => resolve(all)))
+            .then(() => Promise.all(posPromise).then(all => resolve(all)))
             .catch(reason => reject(reason));
     });
 }
